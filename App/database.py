a465a8ecd891ebfa15ca478ffe9f809f832d0666
Mostafa Sayed <moustafasayed068@gmail.com>
2026-02-08 20:42:33 +0000

    conf file

--- App/database.py
+++ App/database.py
@@ -1,10 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-import os
+from pydantic_settings import BaseSettings
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")  
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {})
+class Settings(BaseSettings):
+    DATABASE_URL: str
+
+    class Config:
+        env_file = ".env"
+
+
+
+engine = create_engine(Settings().DATABASE_URL, connect_args={"check_same_thread": False} if Settings().DATABASE_URL.startswith("sqlite") else {})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
