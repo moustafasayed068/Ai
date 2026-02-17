a118e42aab663d2e3ffdda3c08952046ac804f85
Mostafa Sayed <moustafasayed068@gmail.com>
2026-02-17 11:27:23 +0000

    refactor: migrate User id from Integer to UUID

--- App/models/item.py
+++ App/models/item.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from App.db.base import Base
+from sqlalchemy.dialects.postgresql import UUID
 
 class Item(Base):
     __tablename__ = "items"
@@ -9,5 +10,5 @@ class Item(Base):
     title = Column(String, index=True)
     description = Column(String, nullable=True)
 
-    owner_id = Column(Integer, ForeignKey("users.id"))
+    owner_id = Column(UUID(as_uuid=True), ForeignKey("users.id"))
     owner = relationship("User", back_populates="items")
--- App/models/user.py
+++ App/models/user.py
@@ -1,14 +1,16 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from App.db.base import Base
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
     name = Column(String, index=True)
     age = Column(Integer)
-    email = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String(255), unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
 
     items = relationship("Item", back_populates="owner", cascade="all, delete")
